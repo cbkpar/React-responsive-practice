700b9d7ede45da9cd66da80693ff318c42f9605d
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-10-26 15:07:33 +0900

    Responsive for Width

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Page1 from "./pages/Page1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <header className="header"><h1>Responsive Practice</h1></header>
+      <nav></nav>
+      <aside></aside>
+      <section>
+        <Page1></Page1>
+      </section>
+      <footer></footer>
     </div>
   );
 }
---
+++ src/pages/Page1.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Page1.css';
+import { useMediaQuery } from 'react-responsive'
+
+function Page1() {
+
+  const Widedesktop = ({ children }) => {
+    const isWideDesktop = useMediaQuery({ minWidth: 1280 })
+    return isWideDesktop ? children : null
+  }
+  const Desktop = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 1024, maxWidth: 1279 })
+    return isDesktop ? children : null
+  }
+  const Laptop = ({ children }) => {
+    const isLaptop = useMediaQuery({ minWidth: 960, maxWidth: 1023 })
+    return isLaptop ? children : null
+  }
+  const Tablet = ({ children }) => {
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 959 })
+    return isTablet ? children : null
+  }
+  const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+    return isMobile ? children : null
+  }
+
+  return (
+    <div>
+      <div className="box">
+        <Widedesktop><p>넓은 데스크탑 (너비 1280px 보다 같거나 큼)</p></Widedesktop>
+        <Desktop><p>데스크탑 (너비 1024px 보다 같거나 큼)</p></Desktop>
+        <Laptop><p>노트북 (너비 960px 보다 같거나 큼)</p></Laptop>
+        <Tablet><p>태블릿 (너비 768px 보다 같거나 큼)</p></Tablet>
+        <Mobile><p>모바일 (너비 768px 보다 작음)</p></Mobile>
+      </div>
+    </div>
+  );
+}
+
+export default Page1;
