d16459cc16b893e30358d66846e2badba5662706
cbkpar <56503362+cbkpar@users.noreply.github.com>
2021-10-30 20:15:30 +0900

    banner menu

--- src/pages/Page8.js
+++ src/pages/Page8.js
@@ -30,6 +30,36 @@ class Page8 extends Component {
             </div>
           </div>
         </header>
+        {/* 배너 */}
+        <section id="banner">
+          <div className="banner_menu">
+            <div className="container">
+              <div className="row">
+                <div className="bm_left">
+                  <ul>
+                    <li className="total"><a href="#">전체메뉴</a></li>
+                    <li className="line"><a href="#">필름 소사이어티</a></li>
+                    <li><a href="#">클래식 소사이어티</a></li>
+                  </ul>
+                </div>
+                <div className="bm_right">
+                  <ul>
+                    <li className="line"><a href="#">고객센터</a></li>
+                    <li className="line"><a href="#">멤버십</a></li>
+                    <li><a href="#">VIP</a></li>
+                  </ul>
+                  <ul>
+                    <li className="white"><a href="#">상영시간표</a></li>
+                    <li className="purple"><a href="#">빠른 예매</a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="slider">
+            <img src={process.env.PUBLIC_URL + '/img/movie_slider1.jpg'} alt="slider1"></img>
+          </div>
+        </section>
       </div>
     );
   }
